d94e318c5cacd33c9d24ea2e7667875d724ab702
Tyler Melton <meltontyler14@gmail.com>
2024-02-16 12:12:59 -0500

    Add Base Plugin Files

---
+++ themewrath-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Author: T.M. Wrath
+    Author URI: https://tmwrath.com
+    Plugin Name: ThemeWrath Plugin
+    Plugin URI: https://github.com/tmwrath/ThemeWrath-Plugin
+    Description: ThemeWrath Plugin
+    Version: 1.0
+
+*/
